d5709842b7ccf2c667545b4bcf2ae126205a8181
JoshuaRosado <joshua06rosado@gmail.com>
2025-05-13 15:30:54 -0500

    comments added

--- FriendFace/ContentView.swift
+++ FriendFace/ContentView.swift
@@ -5,13 +5,24 @@
 //  Created by Joshua Rosado Olivencia on 5/10/25.
 //
 
+
+
+//            async = “This takes time, don’t freeze everything else.”
+//            await = “Pause here until this part is done.”
+            
+            
+//            async = This function might take some time, like                  cooking or downloading.
+            
+//            await = Wait for something to finish, but don’t stop              everything while we wait.
+
 import SwiftUI
 
 struct ContentView: View {
     @State private var users = [User]()
     var body: some View {
-        
-        List(users, id: \.id) { user in
+        // Display a list of an array of users, using ids
+        List(users, id: \.id) { user in // a user coming in (each one with a unque ID
+            
             VStack{
                 Section("Name"){
                     Text(user.name)
@@ -23,27 +34,38 @@ struct ContentView: View {
                 }
             }
         }
+        
+        
         .task{
+            // Here's a task to do when App is open
+            // wait until data is loaded but don't freeze the app while we wait
             await loadData()
             
-            
-            
+
         }
 
         
     }
-    func loadData() async{
+    func loadData() async { // this takes time, don't freeze everything else
+        
+        
+        // Check if URL is valid
         guard let url = URL(string:"https://www.hackingwithswift.com/samples/friendface.json") else {
             print("Invalid URL")
             return
         }
             do {
+                
+                // try waiting for the data to download
                 let (data, _) = try await URLSession.shared.data(from: url)
                 
+                // turn data into a User object
                 let decodedUser = try JSONDecoder().decode([User].self, from: data )
+                // add new User object to array
                     users = decodedUser
                 
             } catch {
+                // if anything goes wrong
                 print("Invalid data")
             }
 
